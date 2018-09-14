4abcd632ccca456c630705144e94cedd28e64e0c
platon.yasev <yasevplaton@gmail.com>
2018-09-14 12:01:10 +0300

    extract some auxiliary functions to a separate file

--- linear-cartodiagram/js/app.js
+++ linear-cartodiagram/js/app.js
@@ -15,12 +15,9 @@
 
     // prevent submit action
     for (let b of arrayButtons) {
-        b.addEventListener('click', handleClick);
+        b.addEventListener('click', handleClickButton);
     }
 
-    function handleClick(event) {
-        event.preventDefault();
-    }
 
     map.on('load', () => {
 
@@ -28,195 +25,46 @@
         Promise.all([
             fetch('./data/edges4326.geojson?ass=' + Math.random()).then(response => response.json()),
             fetch('./data/nodes4326.geojson?ass=' + Math.random()).then(response => response.json())
-            ]).then(([edges, nodes]) => {
+        ]).then(([edges, nodes]) => {
 
             // set constants for some properties
             const widthArray = [0, 2, 6, 10];
             const colorsArray = { "chocolate": '#661a00', "bananas": '#ffff00', "oranges": '#ff751a' };
             const origLineWidth = 2;
 
-            // function to add colors to edges
-            function addColors(colors) {
-                edges.features.forEach(f => {
-                    if (f.properties.type === "chocolate") {
-                        f.properties.color = colors.chocolate;
-                    } else if (f.properties.type === "bananas") {
-                        f.properties.color = colors.bananas;
-                    } else if (f.properties.type === "oranges") {
-                        f.properties.color = colors.oranges;
-                    };
-                });
-            }
-
-            // function to calculate width of edge
-            function calculateWidth(width) {
-                edges.features.forEach(f => {
-                    if (f.properties.value === 0) {
-                        f.properties.width = width[0];
-                    } else if (f.properties.value > 0 && f.properties.value < 5440) {
-                        f.properties.width = width[1];
-                    } else if (f.properties.value >= 5440 && f.properties.value < 10880) {
-                        f.properties.width = width[2];
-                    } else if (f.properties.value >= 10880) {
-                        f.properties.width = width[3];
-                    }
-                });
-            }
-
-            // function to calculate offset of edge
-            function calculateOffset(origLineWidth) {
-                for (var i = 0; i < edges.features.length; i++) {
-                    if (edges.features[i].properties.order === 0) {
-                        edges.features[i].properties.offset = (origLineWidth / 2) + (edges.features[i].properties.width / 2);
-                    } else {
-                        edges.features[i].properties.offset = edges.features[i - 1].properties.offset +
-                            (edges.features[i - 1].properties.width / 2) + (edges.features[i].properties.width / 2);
-                    }
-                };
-
-            }
+            // create a blank object for storage original lines
+            const origLines = { "type": 'FeatureCollection', features: [] };
 
             // add colors to edges
-            addColors(colorsArray);
+            addColors(edges, colorsArray);
 
             // calculate width of edges
-            calculateWidth(widthArray);
+            calculateWidth(edges, widthArray);
 
             // calculate offset of edges
-            calculateOffset(origLineWidth);
-
-            // create a blank object for storage original lines
-            var origLines = { "type": 'FeatureCollection', features: [] };
-
-
-
-            // function to collect IDs of original lines
-            function collectLinesIDs() {
-
-                var linesIDArray = [];
-
-                edges.features.forEach(e => {
-                    var indexLine = linesIDArray.indexOf(e.properties.ID_line);
-
-                    if (indexLine === -1) {
-                        linesIDArray.push(e.properties.ID_line);
-                    }
-                });
-
-                return linesIDArray;
-            }
-
-
-            // function to get geometry of line by ID
-            function getLineGeometry(lineID) {
-                var geom = {};
-
-                edges.features.forEach(e => {
-                    if (e.properties.ID_line == lineID) {
-                        geom = e.geometry;
-                    }
-                });
-
-                return geom;
-            }
-
-            // function to collect edges that belong to the same original line
-            function collectSameLineEdges(lineID) {
-
-                var sameLineEdges = [];
-
-                edges.features.forEach(e => {
-                    if (e.properties.ID_line === lineID) {
-                        sameLineEdges.push(e);
-                    }
-                })
-
-                return sameLineEdges;
-            }
-
-            // function to calculate width of the widest side of specific original line
-            function calculateSumWidth(lineID) {
-
-                var sameLineEdges = collectSameLineEdges(lineID);
-                let sumWidthFirstSide = 0;
-                let sumWidthSecondSide = 0;
-
-                sameLineEdges.forEach(e => {
-                    if (e.properties.dir === 1) {
-                        sumWidthFirstSide += e.properties.width;
-                    } else if (e.properties.dir === -1) {
-                        sumWidthSecondSide += e.properties.width;
-                    }
-                });
-
-                return sumWidthFirstSide >= sumWidthSecondSide ? sumWidthFirstSide : sumWidthSecondSide;
-            }
-
+            calculateOffset(edges, origLineWidth);
 
             // collect ids of lines
-            var linesIDArray = collectLinesIDs();
+            var linesIDArray = collectLinesIDs(edges);
 
             // calculate width of the widest side of each band in pixels and add to specific property in origLine object
             linesIDArray.forEach(id => {
-                var sumWidth = calculateSumWidth(id) + (origLineWidth / 2);
+                var sumWidth = calculateSumWidth(edges, id) + (origLineWidth / 2);
 
                 var origLine = {
                     properties: {
                         lineID: id,
                         sumWidth: sumWidth
                     },
-                    geometry: getLineGeometry(id)
+                    geometry: getLineGeometry(edges, id)
                 };
 
                 origLines.features.push(origLine);
             });
 
-            // function to find lines that adjacent to specific node
-            function findAdjacentLines(nodeID) {
-                var adjacentLines = [];
-
-                edges.features.forEach(e => {
-                    if (e.properties.src === nodeID || e.properties.dest === nodeID) {
-                        if (adjacentLines.indexOf(e.properties.ID_line) === -1) {
-                            adjacentLines.push(e.properties.ID_line);
-                        }
-                    }
-                });
-
-                return adjacentLines;
-            }
-
-            // function to calculate the maximum width of the adjacent line
-            function calculateMaxWidth(origLines, adjacentLines) {
-
-                var widthArray = [];
-
-                adjacentLines.forEach(adjLine => {
-                    origLines.features.forEach(line => {
-                        if (adjLine === line.properties.lineID) {
-                            widthArray.push(line.properties.sumWidth);
-                        }
-                    });
-                });
-
-                var maxWidth = Math.max(...widthArray);
-
-                return maxWidth;
-            }
-
-            // function to calculate node radius
-            function calculateNodeRadius(nodeID) {
-
-                var adjacentLines = findAdjacentLines(nodeID);
-                var maxWidth = calculateMaxWidth(origLines, adjacentLines);
-                var nodeRadius = maxWidth;
-
-                return nodeRadius;
-            }
-
             // calculate adaptive radius of node
             nodes.features.forEach(node => {
-                node.properties.radius = calculateNodeRadius(node.properties.OBJECTID) + 2;
+                node.properties.radius = calculateNodeRadius(edges, origLines, node.properties.OBJECTID) + 2;
             });
 
             // set sources for map
---
+++ linear-cartodiagram/js/tools.js
@@ -0,0 +1,150 @@
+
+// function to prevent submit action when user clicks on button
+function handleClickButton(event) {
+    event.preventDefault();
+}
+
+// function to add colors to edges
+function addColors(edges, colors) {
+    edges.features.forEach(f => {
+        if (f.properties.type === "chocolate") {
+            f.properties.color = colors.chocolate;
+        } else if (f.properties.type === "bananas") {
+            f.properties.color = colors.bananas;
+        } else if (f.properties.type === "oranges") {
+            f.properties.color = colors.oranges;
+        };
+    });
+}
+
+// function to calculate width of edge
+function calculateWidth(edges, width) {
+    edges.features.forEach(f => {
+        if (f.properties.value === 0) {
+            f.properties.width = width[0];
+        } else if (f.properties.value > 0 && f.properties.value < 5440) {
+            f.properties.width = width[1];
+        } else if (f.properties.value >= 5440 && f.properties.value < 10880) {
+            f.properties.width = width[2];
+        } else if (f.properties.value >= 10880) {
+            f.properties.width = width[3];
+        }
+    });
+}
+
+// function to calculate offset of edge
+function calculateOffset(edges, origLineWidth) {
+    for (var i = 0; i < edges.features.length; i++) {
+        if (edges.features[i].properties.order === 0) {
+            edges.features[i].properties.offset = (origLineWidth / 2) + (edges.features[i].properties.width / 2);
+        } else {
+            edges.features[i].properties.offset = edges.features[i - 1].properties.offset +
+                (edges.features[i - 1].properties.width / 2) + (edges.features[i].properties.width / 2);
+        }
+    };
+
+}
+
+// function to collect IDs of original lines
+function collectLinesIDs(edges) {
+
+    var linesIDArray = [];
+
+    edges.features.forEach(e => {
+        var indexLine = linesIDArray.indexOf(e.properties.ID_line);
+
+        if (indexLine === -1) {
+            linesIDArray.push(e.properties.ID_line);
+        }
+    });
+
+    return linesIDArray;
+}
+
+// function to get geometry of line by ID
+function getLineGeometry(edges, lineID) {
+    var geom = {};
+
+    edges.features.forEach(e => {
+        if (e.properties.ID_line == lineID) {
+            geom = e.geometry;
+        }
+    });
+
+    return geom;
+}
+
+// function to collect edges that belong to the same original line
+function collectSameLineEdges(edges, lineID) {
+
+    var sameLineEdges = [];
+
+    edges.features.forEach(e => {
+        if (e.properties.ID_line === lineID) {
+            sameLineEdges.push(e);
+        }
+    })
+
+    return sameLineEdges;
+}
+
+// function to calculate width of the widest side of specific original line
+function calculateSumWidth(edges, lineID) {
+
+    var sameLineEdges = collectSameLineEdges(edges, lineID);
+    let sumWidthFirstSide = 0;
+    let sumWidthSecondSide = 0;
+
+    sameLineEdges.forEach(e => {
+        if (e.properties.dir === 1) {
+            sumWidthFirstSide += e.properties.width;
+        } else if (e.properties.dir === -1) {
+            sumWidthSecondSide += e.properties.width;
+        }
+    });
+
+    return sumWidthFirstSide >= sumWidthSecondSide ? sumWidthFirstSide : sumWidthSecondSide;
+}
+
+// function to find lines that adjacent to specific node
+function findAdjacentLines(edges, nodeID) {
+    var adjacentLines = [];
+
+    edges.features.forEach(e => {
+        if (e.properties.src === nodeID || e.properties.dest === nodeID) {
+            if (adjacentLines.indexOf(e.properties.ID_line) === -1) {
+                adjacentLines.push(e.properties.ID_line);
+            }
+        }
+    });
+
+    return adjacentLines;
+}
+
+// function to calculate the maximum width of the adjacent line
+function calculateMaxWidth(origLines, adjacentLines) {
+
+    var widthArray = [];
+
+    adjacentLines.forEach(adjLine => {
+        origLines.features.forEach(line => {
+            if (adjLine === line.properties.lineID) {
+                widthArray.push(line.properties.sumWidth);
+            }
+        });
+    });
+
+    var maxWidth = Math.max(...widthArray);
+
+    return maxWidth;
+}
+
+// function to calculate node radius
+function calculateNodeRadius(edges, origLines, nodeID) {
+
+    var adjacentLines = findAdjacentLines(edges, nodeID);
+    var maxWidth = calculateMaxWidth(origLines, adjacentLines);
+    var nodeRadius = maxWidth;
+
+    return nodeRadius;
+}
