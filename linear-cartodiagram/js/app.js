cc495ff5fc0ce432e057eca5259af4e5e7ae7015
platon.yasev <yasevplaton@gmail.com>
2018-11-21 22:37:56 +0300

    change url to edges

--- linear-cartodiagram/js/app.js
+++ linear-cartodiagram/js/app.js
@@ -60,14 +60,14 @@ onLoad = () => {
             Promise.all([
 
                 // edges for production
-                // fetch(url, {
-                //     method: 'POST',
-                //     body: inputFileElement.files[0]
-                // }).then(response => response.json()),
+                fetch(url, {
+                    method: 'POST',
+                    body: inputFileElement.files[0]
+                }).then(response => response.json()),
 
                 // edges for testing
-                fetch('data/edges4326.geojson?ass=' + Math.random())
-                    .then(response => response.json()),
+                // fetch('data/edges4326.geojson?ass=' + Math.random())
+                //     .then(response => response.json()),
 
                 // nodes
                 fetch('data/nodes4326.geojson?ass=' + Math.random())
